72e8613d7995a016b1b1fb0295a10f2091d6e39c
McM1k-home <gboudrie@student.42.fr>
2019-09-30 18:06:14 +0200

    Failed attempt at making pair plot with plotlib

--- src/plot.rs
+++ src/plot.rs
@@ -12,18 +12,26 @@ use plotlib::view::View;
 use plotlib::style::*;
 use strum::IntoEnumIterator;
 use crate::student::Features::*;
+use std::alloc::handle_alloc_error;
 
 pub fn histogram(students: Vec<Student>) {
-    let v = get_histos_feature(
+    let h = get_histos_feature(
         students.clone(),
         Creatures //arithmancy and creatures  are good candidates
     );
+
+    let v = ContinuousView::new()
+        .add(&h[3])
+        .add(&h[2])
+        .add(&h[0])
+        .add(&h[1])
+        .x_label("Creatures repartition");
 
     Page::single(&v).save("histogram.svg").expect("Unable to generate histogram.svg");
     println!("Wrote histogram.svg!");
 }
 
-fn get_histos_feature(students: Vec<Student>, feature: Features) -> ContinuousView {
+fn get_histos_feature(students: Vec<Student>, feature: Features) -> Vec<Histogram> {
     let mut vec = Vec::new();
 
     for house in House::iter() {
@@ -38,26 +46,35 @@ fn get_histos_feature(students: Vec<Student>, feature: Features) -> ContinuousVi
         vec.push(h);
     }
 
-    let label = format!("grade repartition in {}", feature.str());
-    view_houses(vec)
-        .x_label(label.as_str())
+    vec
+//    let label = format!("grade repartition in {}", feature.str());
+//    view_houses(vec)
+//        .x_label(label.as_str())
 }
 
 pub fn scatter(students: Vec<Student>) {
     let ft1 = Defense;
     let ft2 = Astronomy;
 
-    let v = get_scatters_feature(
+    let s = get_scatters_feature(
         students.clone(),
         ft1.clone(),
         ft2.clone()
     );
 
+    let v = ContinuousView::new()
+        .add(&s[3])
+        .add(&s[2])
+        .add(&s[0])
+        .add(&s[1])
+        .x_label(ft1.str())
+        .y_label(ft2.str());
+
     Page::single(&v).save("scatter.svg").expect("Unable to generate scatter.svg");
     println!("Wrote scatter.svg!");
 }
 
-fn get_scatters_feature(students: Vec<Student>, ft1: Features, ft2: Features) -> ContinuousView{
+fn get_scatters_feature(students: Vec<Student>, ft1: Features, ft2: Features) -> Vec<Scatter>{
     let mut vec = Vec::new();
 
     for house in House::iter() {
@@ -70,49 +87,83 @@ fn get_scatters_feature(students: Vec<Student>, ft1: Features, ft2: Features) ->
         vec.push(s);
     }
 
-    view_houses(vec)
-        .x_label(ft1.str())
-        .y_label(ft2.str())
+    vec
+//    view_houses(vec)
+//        .x_label(ft1.str())
+//        .y_label(ft2.str())
 }
 
 pub fn pair(students: Vec<Student>) {
-    let vec = get_pair_line(students);
-
-    let p = Page::empty();
-    for v in vec {
-        p.add_plot(&v);
-    }
-    p.save("pair.svg").expect("Unable to generate pair.svg");
-    println!("Wrote scatter.svg");
+    let s = get_all_scatter(students.clone());
+    let h = get_all_histos(students);
+    big_oof(s, h);
 }
 
-fn get_pair_line(students: Vec<Student>) -> Vec<ContinuousView>{
+fn big_oof(s: Vec<Vec<Scatter>>, h: Vec<Vec<Histogram>>) {
+    Page::empty()
+        .dimensions(4000, 4000)
+
+    //FT1 = ARITHMANCY
+        .add_plot(&ContinuousView::new().add(&h[0][3]).add(&h[0][2]).add(&h[0][0]).add(&h[0][1]).x_label(Arithmancy.str()))
+        .add_plot(&ContinuousView::new().add(&s[1][3]).add(&s[1][2]).add(&s[1][0]).add(&s[1][1]).x_label(Arithmancy.str()).y_label(Astronomy.str()))
+        .add_plot(&ContinuousView::new().add(&s[2][3]).add(&s[2][2]).add(&s[2][0]).add(&s[2][1]).x_label(Arithmancy.str()).y_label(Charms.str()))
+        .add_plot(&ContinuousView::new().add(&s[3][3]).add(&s[3][2]).add(&s[3][0]).add(&s[3][1]).x_label(Arithmancy.str()).y_label(Creatures.str()))
+
+        .save("pair.svg").expect("Unable to generate pair.svg");
+    println!("Wrote pair.svg");
+}
+
+fn get_all_histos(students: Vec<Student>) -> Vec<Vec<Histogram>> {
+    let mut vec = Vec::new();
+
+    for ft in Features::iter() {
+        let h = get_histos_feature(students.clone(), ft);
+        vec.push(h);
+    }
+
+    vec
+}
+
+fn get_all_scatter(students: Vec<Student>) -> Vec<Vec<Scatter>> {
     let mut vec = Vec::new();
 
     for ft1 in Features::iter() {
         for ft2 in Features::iter() {
-            let v;
-            if ft1 == ft2 {
-                let label = format!("grade repartition in {}", ft1.str());
-                v = get_histos_feature(students.clone(), ft1)
-                    .x_label(label.as_str());
-            }
-            else {
-               v = get_scatters_feature(students.clone(), ft1, ft2)
-                   .x_label(ft1.str())
-                   .y_label(ft2.str());
-            }
-            vec.push(v);
+            let s = get_scatters_feature(students.clone(), ft1.clone(), ft2);
+            vec.push(s);
         }
     }
 
     vec
 }
 
-fn view_houses(vec: Vec<_P>) -> ContinuousView {
-    ContinuousView::new()
-        .add(&vec[3])
-        .add(&vec[2])
-        .add(&vec[0])
-        .add(&vec[1])
-}
+//fn get_pair_line(students: Vec<Student>) -> Vec<ContinuousView>{
+//    let mut vec = Vec::new();
+//
+//    for ft1 in Features::iter() {
+//        for ft2 in Features::iter() {
+//            let v;
+//            if ft1 == ft2 {
+//                let label = format!("grade repartition in {}", ft1.str());
+//                v = get_histos_feature(students.clone(), ft1)
+//                    .x_label(label.as_str());
+//            }
+//            else {
+//               v = get_scatters_feature(students.clone(), ft1, ft2)
+//                   .x_label(ft1.str())
+//                   .y_label(ft2.str());
+//            }
+//            vec.push(v);
+//        }
+//    }
+//
+//    vec
+//}
+
+//fn view_houses(vec: Vec<_P>) -> ContinuousView {
+//    ContinuousView::new()
+//        .add(&vec[3])
+//        .add(&vec[2])
+//        .add(&vec[0])
+//        .add(&vec[1])
+//}
