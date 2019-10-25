945469ea24d3639734387d984c5fb89199325f6a
gboudrie <gboudrie@student.42fr>
2019-10-25 16:31:04 +0200

    WIP

--- src/train.rs
+++ src/train.rs
@@ -3,3 +3,16 @@ use crate::student::{Student, Hand, House};
 pub fn train(students: Vec<Student>) {
     //TODO
 }
+
+pub fn sigmoid(z: f64) -> f64 {
+    1.0 / (1.0 + exp(z * -1.0))
+}
+
+pub fn loss(theta: f64, students: Vec<Student>) -> f64 {
+    let m = students.len();
+    let mut sum;
+    for student in students {
+        sum +=
+    }
+    (-1.0 / m as f64) * sum
+}
