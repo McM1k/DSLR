6c0ca6eb249ea6456b5ba5d1b340d820325f291c
gboudrie <gboudrie@student.42fr>
2019-10-26 18:54:20 +0200

    WIP

--- src/train.rs
+++ src/train.rs
@@ -1,4 +1,4 @@
-use crate::student::{Student, Hand, House};
+use crate::student::{Student, Hand, House, Features};
 
 pub fn train(students: Vec<Student>) {
     //TODO
@@ -8,11 +8,38 @@ pub fn sigmoid(z: f64) -> f64 {
     1.0 / (1.0 + exp(z * -1.0))
 }
 
-pub fn loss(theta: f64, students: Vec<Student>) -> f64 {
+pub fn h(thetas: Vec<f64>, student: Student) -> f64 {
+    let mut result = thetas[0];
+    let mut i = 1;
+    for ft in Features {
+        result = result + (ft.func()(&student) * thetas[i]);
+        i = i + 1;
+    }
+    sigmoid(result)
+}
+
+pub fn loss(thetas: Vec<f64>, students: Vec<Student>, house: House) -> f64 {
     let m = students.len();
-    let mut sum;
-    for student in students {
-        sum +=
+    let mut sum= 0.0;
+    for x in students {
+        let y = match student.house {
+            house => 1.0,
+            _ => 0.0,
+        };
+        sum += y * log(h(thetas, x)) + (1.0 - y) * log(1.0 - h(thetas, x))
+    }
+    (-1.0 / m as f64) * sum
+}
+
+pub fn deriv(thetas: Vec<f64>, students: Vec<Student>, house: House, ft: Features) -> f64 {
+    let m = students.len();
+    let mut sum = 0.0;
+    for x in students {
+        let y = match student.house {
+            house => 1.0,
+            _ => 0.0,
+        };
+        sum = sum + (h(thetas, student) - y) * ft.func()(&x);
     }
     (-1.0 / m as f64) * sum
 }
